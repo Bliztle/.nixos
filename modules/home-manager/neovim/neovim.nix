53801cb1b7cee3b1a1d70a91f8f3aa9f32d6417c
Bliztle <asbjoern.r.e@gmail.com>
2024-03-25 22:32:36 +0100

    update and nvim

--- modules/home-manager/neovim/neovim.nix
+++ modules/home-manager/neovim/neovim.nix
@@ -5,8 +5,15 @@
   config = {
     programs.neovim = {
       enable = true;
+      viAlias = true;
+      vimAlias = true;
+      # package = pkgs.neovim-nightly;
 
       plugins = with pkgs.vimPlugins; [
+      	LazyVim
+	lazy-nvim
+	nvim-treesitter
+	nvim-treesitter.withAllGrammars # What is this dark magic?
         auto-pairs
         nui-nvim # nvim ui components used by neo-tree-nvim
         plenary-nvim # nvim lua library used by telescope and neo-tree-nvim
@@ -16,6 +23,28 @@
         neo-tree-nvim
         which-key-nvim
       ];
+
+      extraPackages = with pkgs; [
+      	gcc
+      ];
+      extraLuaConfig = ''
+      	vim.g.mapleader = " " -- Set leader before lazy for correct keybindings;
+      	require("lazy").setup({
+	  performance = {
+	    reset_packpath = false,
+	    rtp = {
+  	      reset = false,
+	    }
+	  },
+	  dev = {
+	    path = path = "${pkgs.vimUtils.packDir config.programs.neovim.finalPackage.passthru.packpathDirs}/pack/myNeovimPackages/start",
+	  },
+	  install = {
+	    -- Safeguard in case we forget to install a plugin with Nix
+	    missing = false,
+	  }
+	})
+      '';
     };
   };
 }
