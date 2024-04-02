981acb6542f347c4aca3c53eafb22e1487b68221
Bliztle <asbjoern.r.e@gmail.com>
2024-04-02 23:59:40 +0200

    enable work module on zenbook

--- hosts/zenbook/home.nix
+++ hosts/zenbook/home.nix
@@ -11,6 +11,7 @@
 
   custom.unstable.enable = false;
   custom.dev.enable = true;
+  custom.work.enable = true;
 
   # This value determines the Home Manager release that your configuration is
   # compatible with. This helps avoid breakage when a new Home Manager release
