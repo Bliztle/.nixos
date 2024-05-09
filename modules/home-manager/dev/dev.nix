0e8d8e0ea04d17ba5311ae16669c35f9da13e49a
Asbjørn Rysgaard Eriksen <mail@bliztle.com>
2024-05-09 23:10:53 +0200

    base dev languages + lsps and flake update

--- modules/home-manager/dev/dev.nix
+++ modules/home-manager/dev/dev.nix
@@ -1,7 +1,8 @@
-{ lib, config, pkgs, ... }:
+{ lib, inputs, config, pkgs, ... }:
 
 let
   cfg = config.custom.dev;
+  unstable = inputs.nixpkgs-unstable.legacyPackages.${pkgs.system};
 in
 {
   options.custom.dev = {
@@ -14,5 +15,34 @@ in
       enableZshIntegration = true;
       nix-direnv.enable = true;
     };
+
+    home.packages = with unstable; [
+      # Nix
+      nil
+      # Lua
+      lua-language-server
+      # C
+      gcc
+      # C#
+      dotnet-sdk_8
+      omnisharp-roslyn
+      # Javascript / Typescript
+      nodejs
+      nodePackages_latest.typescript-language-server
+      # Rust
+      rustc
+      cargo
+      rustfmt
+      clippy
+      evcxr # REPL and Jupyter kernel
+      rust-analyzer
+      # Haskell
+      #haskell.compiler.ghcHEAD
+      #haskell.compiler.native-bignum.ghc965
+      #haskellPackages.ghc
+      haskell.compiler.ghc963
+      haskellPackages.haskell-language-server
+      haskellPackages.stack
+    ];
   };
 }
